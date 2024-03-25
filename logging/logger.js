b79864c156d48bce7f91560ed622fe2fe431425e
Parichay Didwania <parichaydidwania@gmail.com>
2024-03-24 22:19:27 -0400

    winston logging added

---
+++ logging/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+
+const timezone = () => {
+    return new Date().toISOString();
+}
+
+const capitalizeLevels = winston.format(info => {
+    info.level = info.level.toUpperCase();
+    return info;
+})
+
+const Logger = winston.createLogger({
+  format: winston.format.combine (
+    winston.format.timestamp({ format: timezone }),
+    capitalizeLevels(),
+    winston.format.json()
+  ),
+  transports: [new winston.transports.Console({ format: winston.format.json() })]
+});
+
+module.exports = Logger;
